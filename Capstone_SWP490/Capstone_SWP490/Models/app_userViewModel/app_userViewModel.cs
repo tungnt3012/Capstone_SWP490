d93e5609a5586b7216cf0864d85877d2c9e59dd9
tungbeo <tungntse05838@fpt.edu.vn>
2022-01-21 02:50:00 +0700

    add app_userViewmodel

---
+++ Capstone_SWP490/Capstone_SWP490/Models/app_userViewModel/app_userViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_SWP490.Models.app_userViewModel
+{
+    public class app_userViewModel
+    {
+        public int user_id { get; set; }
+        public string user_name { get; set; }
+        public string user_role { get; set; }
+        public string psw { get; set; }
+        public string encrypted_psw { get; set; }
+        public string full_name { get; set; }
+        public string email { get; set; }
+        public bool verified { get; set; }
+        public bool active { get; set; }
+    }
+}
