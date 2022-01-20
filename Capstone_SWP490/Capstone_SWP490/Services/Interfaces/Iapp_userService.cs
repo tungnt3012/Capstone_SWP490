88fed8a5ce89bcd9d5bfa036cc838e43d06aeb0b
tungbeo <tungntse05838@fpt.edu.vn>
2022-01-21 02:46:57 +0700

    add services

---
+++ Capstone_SWP490/Capstone_SWP490/Services/Interfaces/Iapp_userService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone_SWP490.Services.Interfaces
+{
+    public interface Iapp_userService
+    {
+        List<app_user> GetAllUser();
+    }
+}
---
+++ Capstone_SWP490/Capstone_SWP490/Services/app_userService.cs
@@ -0,0 +1,19 @@
+﻿using Capstone_SWP490.Repositories;
+using Capstone_SWP490.Repositories.Interfaces;
+using Capstone_SWP490.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_SWP490.Services
+{
+    public class app_userService : Iapp_userService
+    {
+        private readonly Iapp_userRepository _iapp_UserRepository = new app_userRepository();
+        public List<app_user> GetAllUser()
+        {
+            return _iapp_UserRepository.GetAllApp_Users();
+        }
+    }
+}
