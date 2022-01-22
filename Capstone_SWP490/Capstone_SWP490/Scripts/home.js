d342ae827b63fae0d488392991727d2337a37f91
sondaika1998 <56884289+sondaika1998@users.noreply.github.com>
2022-01-23 00:05:51 +0700

    Update giao dien
    Update giao dien

--- Capstone_SWP490/Capstone_SWP490/Scripts/home.js
+++ Capstone_SWP490/Capstone_SWP490/Scripts/home.js
@@ -5,6 +5,7 @@ $("#menu-toggle").click(function (e) {
 $("#menu-toggle-2").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled-2");
+    $("#content_area2_header").toggleClass("toggled-2");
     $('#menu ul').hide();
 });
 
