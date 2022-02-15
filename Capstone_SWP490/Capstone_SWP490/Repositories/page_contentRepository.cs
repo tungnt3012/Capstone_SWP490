8a2a75fcf9ed45a36f3ef61ef0e1852a6b8fe000
tungbeo <tungntse05838@fpt.edu.vn>
2022-02-15 17:07:59 +0700

    update fill data from database

--- Capstone_SWP490/Capstone_SWP490/Repositories/page_contentRepository.cs
+++ Capstone_SWP490/Capstone_SWP490/Repositories/page_contentRepository.cs
@@ -9,5 +9,13 @@ namespace Capstone_SWP490.Repositories
 {
     public class page_contentRepository : GenericRepository<page_content>, Ipage_contentRepository
     {
+        public List<page_content> GetPage_ContentByPageId(string stringId)
+        {
+            //var lstPage_Content = FindBy(x => x.page_id.Equals(stringId)).ToList();
+            var rs = (from data in FindBy(x => x.page_id.Equals(stringId)).ToList()
+                      orderby data.position ascending
+                     select data).ToList();
+            return rs;
+        }
     }
 }
