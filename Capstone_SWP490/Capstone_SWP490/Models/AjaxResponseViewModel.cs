cdb2d110133502b814b46b784c419f73a08f5583
tungbeo <tungntse05838@fpt.edu.vn>
2022-02-21 12:27:02 +0700

    add Response ViewModel

---
+++ Capstone_SWP490/Capstone_SWP490/Models/AjaxResponseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_SWP490.Models
+{
+    public class AjaxResponseViewModel<T>
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
