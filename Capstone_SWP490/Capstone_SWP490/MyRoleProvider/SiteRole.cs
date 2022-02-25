abefff501d96eefb2a35eb46e2180e926e241ba7
tungbeo <tungntse05838@fpt.edu.vn>
2022-02-25 21:13:49 +0700

    add SiteRole

---
+++ Capstone_SWP490/Capstone_SWP490/MyRoleProvider/SiteRole.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace Capstone_SWP490.MyRoleProvider
+{
+    public class SiteRole : RoleProvider
+    {
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public MyRoleProvider() 
+    {}
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            gocyberx_icpcEntities db = new gocyberx_icpcEntities();
+            string data = db.app_user.Where(x => x.user_name == username).FirstOrDefault().user_role;
+            string[] result = { data };
+            return result;
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            string[] roles = GetRolesForUser(username);
+            foreach (string role in roles)
+            {
+                if (roleName.Equals(role, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
