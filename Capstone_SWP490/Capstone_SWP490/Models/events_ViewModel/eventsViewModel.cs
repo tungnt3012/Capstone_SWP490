23b7779422da8d71f5b59e4381c6b2c6bf56244e
tungbeo <tungntse05838@fpt.edu.vn>
2022-03-10 10:35:58 +0700

    Add eventViewmodel

---
+++ Capstone_SWP490/Capstone_SWP490/Models/events_ViewModel/eventsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_SWP490.Models.events_ViewModel
+{
+    public class eventsViewModel
+    {
+        public int event_id { get; set; }
+        public short event_type { get; set; }
+        public string title { get; set; }
+        public string desctiption { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime end_date { get; set; }
+        public string venue { get; set; }
+        public string contactor_name { get; set; }
+        public string contactor_email { get; set; }
+        public string contactor_phone { get; set; }
+        public string fan_page { get; set; }
+        public Nullable<short> shirt_id { get; set; }
+        public string note { get; set; }
+    }
+}
