4cf2335f727e9a7ebc28c84184f4952bed40705f
tungbeo <tungntse05838@fpt.edu.vn>
2022-03-10 10:36:24 +0700

    pagingOutputModel

---
+++ Capstone_SWP490/Capstone_SWP490/Models/PagingOutput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_SWP490.Models
+{
+    public class PagingOutput <T>
+    {
+        public int Index { get; set; }
+        public int PageSize { get; set; }
+        public int TotalItem { get; set; }
+        public int TotalPage { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
