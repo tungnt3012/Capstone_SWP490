e2ade29569abffd1cd347046a9982ca304aa98fe
anhttse06009 <anhttse06009@fpt.edu.vn>
2022-03-12 06:56:03 +0700

    fix paging coach account

---
+++ Capstone_SWP490/Capstone_SWP490/Models/school_memberViewModel/member_contest_ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_SWP490.Models.school_memberViewModel
+{
+    public class member_contest_ViewModel
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+        public bool selected { get; set; }
+    }
+}
---
+++ Capstone_SWP490/Capstone_SWP490/Models/school_memberViewModel/member_detail_ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_SWP490.Models.school_memberViewModel
+{
+    public class member_detail_ViewModel
+    {
+        public member_detail_ViewModel()
+        {
+            errors = new Dictionary<string, string>();
+        }
+        public int team_id { get; set; }
+        public int member_id { get; set; }
+        public bool is_leader { get; set; }
+        public string first_name { get; set; }
+        public string middle_name { get; set; }
+        public string last_name { get; set; }
+        public System.DateTime dob { get; set; }
+        public string email { get; set; }
+        public string phone_number { get; set; }
+        public short gender { get; set; }
+        public int? year { get; set; }
+        public string award { get; set; }
+        public int? icpc_id { get; set; }
+
+        public Dictionary<string, string> errors { get; set; }
+        public List<member_contest_ViewModel> contest_Members { get; set; }
+    }
+}
