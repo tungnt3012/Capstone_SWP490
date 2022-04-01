b2a6c391405190a307bc2c1543daf000c8fda904
tungbeo <tungntse05838@fpt.edu.vn>
2022-04-01 18:52:42 +0700

    update entities

--- Capstone_SWP490/Capstone_SWP490/icpc.Designer.cs
+++ Capstone_SWP490/Capstone_SWP490/icpc.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Admin\Desktop\Capstone_SWP490\Capstone_SWP490\Capstone_SWP490\icpc.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\AD\OneDrive\Máy tính\demo\icpc.12.3\Capstone_SWP490\Capstone_SWP490\icpc.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
