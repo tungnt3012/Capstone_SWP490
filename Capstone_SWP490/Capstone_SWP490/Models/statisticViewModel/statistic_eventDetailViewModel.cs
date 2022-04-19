cdd063cda38c7086fd1ad7e52de7e626c97e11dc
tungbeo <tungntse05838@fpt.edu.vn>
2022-04-19 08:12:20 +0700

    add viewmodel

---
+++ Capstone_SWP490/Capstone_SWP490/Models/statisticViewModel/statistic_eventDetailViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Capstone_SWP490.Models.events_ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_SWP490.Models.statisticViewModel
+{
+    public class statistic_eventDetailViewModel
+    {
+        public int event_id { get; set; }
+        public short event_type { get; set; }
+        public string title { get; set; }
+        public string desctiption { get; set; }
+        public System.DateTime start_date { get; set; }
+        public System.DateTime end_date { get; set; }
+        public string venue { get; set; }
+        public string contactor_name { get; set; }
+        public string contactor_email { get; set; }
+        public string contactor_phone { get; set; }
+        public string fan_page { get; set; }
+        public Nullable<short> shirt_id { get; set; }
+        public string note { get; set; }
+        public string member_join { get; set; }
+        public string sub_event { get; set; }
+        public Nullable<int> status { get; set; }
+        public List<member> lstMembers { get; set; }
+    }
+}
---
+++ Capstone_SWP490/Capstone_SWP490/Models/statisticViewModel/statistic_eventViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Capstone_SWP490.Models.events_ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_SWP490.Models.statisticViewModel
+{
+    public class statistic_eventViewModel
+    {
+        public int totalEvent { get; set; }
+        public List<eventsViewModel> lstEvents { get; set; }
+    }
+}
