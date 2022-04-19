bb0bad98d54a73749684d5dc85a76b5821d53350
tungbeo <tungntse05838@fpt.edu.vn>
2022-04-19 09:08:57 +0700

    add viewwModel

---
+++ Capstone_SWP490/Capstone_SWP490/Models/statisticViewModel/statistic_shirtSizeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Capstone_SWP490.Models.events_ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_SWP490.Models.statisticViewModel
+{
+    public class statistic_shirtSizeViewModel
+    {
+        public int totalRegisterdSize { get; set; }
+        public int sizeS { get; set; }
+        public int sizeXS { get; set; }
+        public int sizeM { get; set; }
+        public int sizeL { get; set; }
+        public int sizeXL { get; set; }
+        public int size2XL { get; set; }
+        public int size3XL { get; set; }
+        public List<member> lstMembers { get; set; }
+    }
+}
