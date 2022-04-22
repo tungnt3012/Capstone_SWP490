7746da97d8915c9d907607b01fc4d1da33b9593b
tungbeo <tungntse05838@fpt.edu.vn>
2022-04-22 07:54:38 +0700

    add viewmodel

---
+++ Capstone_SWP490/Capstone_SWP490/Models/statisticViewModel/registered_contest_ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_SWP490.Models.statisticViewModel
+{
+    public class registered_contest_ViewModel
+    {
+        public contest contest { get; set; }
+        public List<member> lstMember { get; set; }
+        public int contestant_number { get; set; }
+    }
+}
---
+++ Capstone_SWP490/Capstone_SWP490/Models/statisticViewModel/registered_school_ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_SWP490.Models.statisticViewModel
+{
+    public class registered_school_ViewModel
+    {
+        public school school { get; set; }
+        public int contestant { get; set; }
+        public string coach_name { get; set; }
+        public string coach_phone { get; set; }
+    }
+}
