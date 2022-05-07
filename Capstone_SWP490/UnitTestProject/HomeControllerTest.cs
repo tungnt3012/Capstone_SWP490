b262bf7b308817d6f2a0337e7e7ee85c3cc2c6b0
Anhttse06009 <97394183+Anhttse06009@users.noreply.github.com>
2022-05-07 15:22:16 +0700

    Unit test
    Add Unit Test

---
+++ Capstone_SWP490/UnitTestProject/HomeControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using Capstone_SWP490.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void ShirtSizing()
+        {
+            var controller = new HomeController();
+            var index = controller.ShirtSizing();
+            Assert.IsNotNull(index);
+        }
+        [TestMethod]
+        public void Scoreboard()
+        {
+            var controller = new HomeController();
+            var index = controller.Scoreboard();
+            Assert.IsNotNull(index);
+        }
+        [TestMethod]
+        public void Error()
+        {
+            var controller = new HomeController();
+            var index = controller.Error();
+            Assert.IsNotNull(index);
+        }
+        
+        
+    }
+}
